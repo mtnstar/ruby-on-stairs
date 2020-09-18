5abb36ef03cb0a911c374141fd82261ce71355b5
Pascal Simon <simon@puzzle.ch>
2020-09-18 11:37:21 +0200

    Add commander, add rubocop config

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
+gem 'commander', '~> 4.5'
 gem 'rspec'
--- lib/ruby_on_stairs.rb
+++ lib/ruby_on_stairs.rb
@@ -1,8 +1,28 @@
+# frozen_string_literal: true
+
+require 'commander'
+
 class RubyOnStairs
+  include Commander::Methods
 
-  def climb
+  def run # rubocop:disable Metrics/MethodLength
+    program :name, 'ruby on stairs'
+    program :version, '1.0.0'
+    program :description, 'Organize and execute your ruby code by steps'
+    program :help, 'Source Code', 'https://www.github.com/psunix/ruby-on-stairs'
+    program :help, 'Usage', 'stairs [flags]'
+
+    command :run do |c|
+      c.syntax = 'cry run <step nr>'
+      c.description = 'runs all or single steps/sub-step'
+
+      c.action do |args|
+        TTY::Exit.exit_with(:usage_error, 'Credentials missing') if args.empty?
+        step_nr = args.first
+        execute_action do
+          stairs.run(step_nr)
+        end
+      end
+    end
   end
-
-  private
-
 end
